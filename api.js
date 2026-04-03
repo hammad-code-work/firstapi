1e5f54a0639c23d9cd3949be9b1e0c2216319bef
322068 <322068@FAC-SPORTS.COM>
2026-04-03 11:26:27 +0500

    Initial commit

--- api.js
+++ api.js
@@ -1,27 +1,31 @@
 // 1. Grab the element where we will display the total count
-const countEl = document.getElementById("count"); 
+const countEl = document.getElementById("count");
 
 // 2. Grab the button element that the user will click
-const btn = document.getElementById("load"); 
+const btn = document.getElementById("load");
 
 // 3. Set up an event listener to wait for the click
-btn.addEventListener("click", async () => { 
-    
+btn.addEventListener("click", async () => {
+
     // 4. Request the user list from the external server (API)
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    
+
     // 5. Convert the raw server response into a readable JavaScript array
-    const data = await res.json(); 
-    
-    // 6. Calculate how many user objects are in the array
-    const count = data.length; 
-    
-    // 7. Update the webpage text to show the total count
-    countEl.innerText = count; 
+    const data = await res.json();
+
+    // 6. Calculate how many user objects are in the array / show counts
+    const count = data.length;
+
+    // 7. Update the webpage text(that is 0 in index.html) to show the total count on screen
+    countEl.innerText = count;
+    //7b. this will prevent from same data repeating.
+     list.innerHTML = "";
 
     // 8. Start a loop to visit every individual user in the list
-    data.forEach(user => { 
-        // 9. Print the 'name' property of each user to the console
-        console.log(user.name); 
-    }); 
+    data.forEach(user => {
+        // 9. (for check in console)Print the 'name' property of each user to the console
+        // console.log(user.name); 
+        //9. for UI 
+        list.innerHTML += `<li>${user.name}</li>`;
+    });
 });
